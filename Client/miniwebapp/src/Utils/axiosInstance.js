dc17e72d73dec220a56a64d0adeaee1060f4bb4f
shahid <ms4811879@gmail.com>
2025-08-26 10:52:53 +0530

    server side deployed api changed

--- Client/miniwebapp/src/Utils/axiosInstance.js
+++ Client/miniwebapp/src/Utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://api-driven-mini-web-app-1.onrender.com/api", 
 });
 
 export default axiosInstance;
