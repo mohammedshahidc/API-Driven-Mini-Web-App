7ae391733fa9c7f2631812c3a47097b6d4766b3e
shahid <ms4811879@gmail.com>
2025-08-26 11:02:03 +0530

    set credentials

--- Client/miniwebapp/src/Utils/axiosInstance.js
+++ Client/miniwebapp/src/Utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://api-driven-mini-web-app-1.onrender.com/api", 
+  baseURL: "https://api-driven-mini-web-app-1.onrender.com/api",
+  withCredentials:true 
 });
 
 export default axiosInstance;
